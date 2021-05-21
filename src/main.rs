9cb1ce5aad1a7a916e47f697a8c3dc341bf1a0df
solidiquis <benjamin.van.nguyen@gmail.com>
2021-05-20 22:18:13 -0700

    Implemented get_winsize

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,13 @@ use keys::watcher;
 
 mod win;
 use win::stty;
+use win::winsize;
 
 fn main() {
+    //let (row, col) = winsize::get_winsize().unwrap_or_else(|err| {
+        //panic!("{}", err)
+    //});
+
     stty::unecho_stdin();
     stty::unbuffer_stdin();
 
--- src/win/mod.rs
+++ src/win/mod.rs
@@ -1,1 +1,2 @@
 pub mod stty;
+pub mod winsize;
