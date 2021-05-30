0a1a0cdb32f0aaa21fdd243afcbd67ae97a9bff5
solidiquis <benjamin.van.nguyen@gmail.com>
2021-05-30 01:53:14 -0700

    WIP

--- src/cache/mod.rs
+++ src/cache/mod.rs
@@ -1,6 +1,6 @@
-struct TextCache {
-    text: Vec<String>,
-    history: Vec<Vec<String>>
+pub struct TextCache {
+    pub text: Vec<String>,
+    pub history: Vec<Vec<String>>
 }
 
 impl Default for TextCache {
@@ -12,12 +12,3 @@ impl Default for TextCache {
     }
 }
 
-impl TextCache {
-    pub fn edit_line(&mut self, line_no: u8, col: u8, txt: &str) {
-        if (self.text.len() as u8) < line_no {
-            self.text.push("".to_string())
-        };
-        
-        self.text[(line_no - 1) as usize].push_str(txt)
-    }
-}
